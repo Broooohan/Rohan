d89bfe800e490ee4468926fbfa97e39acbb5d2af
Broooohan <77372476+Broooohan@users.noreply.github.com>
2021-04-12 12:07:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const tellYear = new Date().getFullYear();
 console.log(tellYear);
 
 document.querySelector("footer p").textContent = "Copyright © " + tellYear;
+
+$('body').ripples({
+    dropRadius: 20,
+    perturbance: 2,
+    resolution: 1000
+  });
